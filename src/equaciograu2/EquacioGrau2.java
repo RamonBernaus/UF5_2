fbfa4ec9344c4bb59b97f1173212fa2e44eeb1b3
ramon <ramon@Ramon>
2022-05-03 20:00:18 +0200

    Act2

--- src/equaciograu2/EquacioGrau2.java
+++ src/equaciograu2/EquacioGrau2.java
@@ -7,36 +7,58 @@ package equaciograu2;
  * @author Ramon
  */
 public class EquacioGrau2 {
-    
-    public EquacioGrau2(int a, int b, int c){
-        
+
+    public EquacioGrau2(int a, int b, int c) {
+
     }
-    
-    private double sol1,sol2;
-    
-    public void setsol1(double sol1){
+
+    private double sol1, sol2;
+
+    public void setsol1(double sol1) {
         this.sol1 = sol1;
     }
-    public void setsol2(double sol2){
+
+    public void setsol2(double sol2) {
         this.sol2 = sol2;
     }
-    public double getsol1(){
-    return sol1;    
+
+    public double getsol1() {
+        return sol1;
     }
-    public double getsol2(){
-    return sol2;    
+
+    public double getsol2() {
+        return sol2;
     }
-    
-    public double arrels(int a, int b, int c) throws Exception{
-        double b_quadrat = Math.sqrt(b*b-4*a*c);
-        double resultat_pos = (-b+(b_quadrat))/(2*a);
-        setsol1(resultat_pos);
-        double resultat_neg = (-b-(b_quadrat))/(2*a);
-        setsol2(resultat_neg);
-    if (a == 0){
-        throw new Exception();
-    }    
-        return 0;
+
+    public String arrels(int a, int b, int c) throws Exception {
+        String str = " ";
+        double b_quadrat = Math.sqrt(b * b - 4 * a * c);
+        sol1 = (-b + (b_quadrat)) / (2 * a);
+        sol2 = (-b - (b_quadrat)) / (2 * a);
+        if (a == 0) {
+            PrimerCoeficientZeroException ex = new PrimerCoeficientZeroException();
+            throw ex;
+        } else if (b * b < 4 * a * c) {
+            NoArrelsRealsException exept = new NoArrelsRealsException();
+            throw exept;
+        } else {
+getsol1();
+getsol2();
+}
+        return str;
     }
-    
+
+    public class PrimerCoeficientZeroException extends Exception {
+
+        PrimerCoeficientZeroException() {
+            super("Error el primer coeficient 'a' es igual a 0");
+        }
+    }
+
+    public class NoArrelsRealsException extends Exception {
+
+        NoArrelsRealsException() {
+            super("Error la arrel mo es una arrel real");
+        }
+    }
 }
--- src/equaciograu2/Menú.java
+++ src/equaciograu2/Menú.java
@@ -8,9 +8,8 @@ package equaciograu2;
 public class Menú {
 
     public static void main(String[] args) throws Exception {
-        int a =2, b = 5, c = 2;
+        int a = 2, b = 5, c = 2;
         EquacioGrau2 e = new EquacioGrau2(a, b, c);
-        double b_quadrat = Math.sqrt(b);
         System.out.println(e.arrels(a, b, c));
     }
 
